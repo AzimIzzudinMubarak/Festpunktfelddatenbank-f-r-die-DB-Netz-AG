ccc11a492ce2e2b802382a58b2cd39b4eeb3085e
AzimIzzudinMubarak <97334916+AzimIzzudinMubarak@users.noreply.github.com>
2022-08-24 14:45:20 +0200

    Add files via upload

---
+++ FestpunktDB.GUI/resources/EntitiesExport/ExportPP.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FestpunktDB.Business.EntitiesExport
+{
+    public partial class ExportPp
+    {
+        public ExportPp()
+        {
+            ExportPh = new HashSet<ExportPh>();
+            ExportPk = new HashSet<ExportPk>();
+            ExportPl = new HashSet<ExportPl>();
+            ExportPs = new HashSet<ExportPs>();
+        }
+
+        public string PAD { get; set; }
+        public string PArt { get; set; }
+        public string Blattschnitt { get; set; }
+        public int? PunktNr { get; set; }
+        public short? VermArt { get; set; }
+        public short? Stabil { get; set; }
+        public string PDatum { get; set; }
+        public string PBearb { get; set; }
+        public string PAuftr { get; set; }
+        public string PProg { get; set; }
+        public string PText { get; set; }
+        public DateTime? Import { get; set; }
+        public DateTime? LoeschDatum { get; set; }
+        public virtual ICollection<ExportPh> ExportPh { get; set; }
+        public virtual ICollection<ExportPk> ExportPk { get; set; }
+        public virtual ICollection<ExportPl> ExportPl { get; set; }
+        public virtual ICollection<ExportPs> ExportPs { get; set; }
+    }
+}
---
+++ FestpunktDB.GUI/resources/EntitiesExport/ExportPS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FestpunktDB.Business.EntitiesExport
+{
+    public partial class ExportPs
+    {
+        public string PAD { get; set; }
+        public string PStrecke { get; set; }
+        public short PSTRRiKz { get; set; }
+        public double? Station { get; set; }
+        public DateTime? Import { get; set; }
+        public DateTime? LoeschDatum { get; set; }
+        public DateTime? SDatum { get; set; }
+
+        public virtual ExportPp PadNavigation { get; set; }
+    }
+}
